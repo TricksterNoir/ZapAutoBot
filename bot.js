57da522d4911513daa5e7eb919d2e1d9e790ce81
sylvio labriola <sylvioez@hotmail.com>
2024-05-17 19:21:40 -0300

    Adicionado o jogo do bicho, removido testes anteriores e código melhorado.

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+// Importa as funções que serão utilizadas
+const { sortearDezenas, sortearJogoDoBicho } = require('./functions');
+
 // Declara e usa as ferramentas
 const { Client, MessageMedia, NoAuth, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
@@ -25,24 +28,20 @@ client.on('message', async (message) => {
     }
 
     if (message.body.toLowerCase() === '!megumin') {
-        const media = MessageMedia.fromFilePath('/images/megumin.jpeg')
+        const media = MessageMedia.fromFilePath('./images/megumin.jpeg')
         const chat = await message.getChat()
         await client.sendMessage(message.from, 'A mais gostosa, simpática e divertida feiticeira de Konosuba');
         chat.sendMessage(media)
     }
 
     if (message.body.toLowerCase() === '!l'){
-        const media = MessageMedia.fromFilePath('/images/fazOL.jpg')
+        const media = MessageMedia.fromFilePath('./images/fazOL.jpg')
         const chat = await message.getChat()
         await client.sendMessage(message.from, 'Mantenha a calma guerreiro, lembre-se você tem culpa nisso, agora siga o conselho da imagem: ')
         chat.sendMessage(media)
     }
 
-    if (message.body === '!isabelly') {
-        await client.sendMessage(message.from, 'Linda, cheirosa, gostosa, peituda, lerda, preguiçosa, uma vaca, meio lélé das ideias, que precisa aprender a usar sombras, mas uma excelente amiga cuja a qual estou com saudades. Te amo sua corna <3');
-    }
-
-    if (message.body === '!preco') {
+    if (message.body.toLowerCase() === '!preco') {
         const listaPrecos = `Abaixo segue a lista de preços do nosso serviço:\n
         Landing Page de vendas: R$1000.00\n
         Landing Page de captura: R$ 850.00\n
@@ -52,24 +51,15 @@ client.on('message', async (message) => {
         await client.sendMessage(message.from, 'Algum dos nossos serviços se enquadra na sua demanda?');
     }
 
-    if (message.body === '!megasena'){
-        function sortearDezenas(){
-            var listaSorteada = [];
-            var i = 0;
-            while (i < 6){
-                var numeroSorteado = Math.floor(Math.random() * 60) + 1;
-                var numeroFormatado = numeroSorteado.toString().padStart(2, '0');
-                if (!listaSorteada.includes(numeroFormatado)){
-                    listaSorteada.push(numeroFormatado);
-                    i++
-                }
-            }
-            return listaSorteada
+     if (message.body.toLowerCase() === '!megasena'){
+         var sorteio = sortearDezenas()
+         await client.sendMessage(message.from, 'Seu palpite da Mega Sena é: ' + sorteio.join(', '));
+     }
 
-        }
-        var sorteio = sortearDezenas()
-        await client.sendMessage(message.from, 'Seu palpite da Mega Sena é: ' + sorteio.join(', '));
-    }
+     if (message.body.toLowerCase() === '!bicho'){
+        var resposta = await sortearJogoDoBicho()
+        await client.sendMessage(message.from, 'Seu palpite para o jogo do bicho é:\n ' + resposta);
+     }
 
 });
 
---
+++ functions.js
@@ -0,0 +1,46 @@
+const fs = require('fs').promises;
+
+function sortearDezenas(){
+    var listaSorteada = [];
+    var i = 0;
+    while (i < 6){
+        var numeroSorteado = Math.floor(Math.random() * 60) + 1;
+        var numeroFormatado = numeroSorteado.toString().padStart(2, '0');
+        if (!listaSorteada.includes(numeroFormatado)){
+            listaSorteada.push(numeroFormatado);
+            i++
+        }
+    }
+    return listaSorteada
+
+}
+
+async function sortearJogoDoBicho() {
+    let numero_completo = '';
+
+    for (let i = 0; i < 5; i++) {
+        const grupo_animal_sorteado_cabeca = Math.floor(Math.random() * 25) + 1;
+        const grupo_animal_sorteado_animal = Math.floor(Math.random() * 25) + 1;
+        let grupo_posicao_numero = Math.floor(Math.random() * 4);
+
+        const data = JSON.parse(await fs.readFile('animal.json', 'utf-8'));
+
+        let animal_selecionado = data.animais.find(animal => animal.numero_grupo === grupo_animal_sorteado_cabeca);
+        const numero_cabeca = animal_selecionado.numeros_animal[grupo_posicao_numero];
+
+        grupo_posicao_numero = Math.floor(Math.random() * 4);
+        animal_selecionado = data.animais.find(animal => animal.numero_grupo === grupo_animal_sorteado_animal);
+        const numero_animal = animal_selecionado.numeros_animal[grupo_posicao_numero];
+        const numero_grupo = animal_selecionado.numero_grupo;
+        const animal_nome = animal_selecionado.animal;
+
+        numero_completo += `${i + 1}º ${numero_cabeca}${numero_animal} (${numero_grupo}) ${animal_nome}\n`;
+    }
+
+    return numero_completo;
+}
+
+
+
+
+module.exports = { sortearDezenas, sortearJogoDoBicho };
