7c1a6c151792c2fc38f63a23b4413fdd2b054574
sylvio labriola <sylvioez@hotmail.com>
2024-05-20 15:52:22 -0300

    Adicionando menção, agora deixo o chrome aberto para acompanhar a execução dos envios e adicionei o envio de URL. Envio de vídeo como arquivo não funcionou.

--- bot.js
+++ bot.js
@@ -2,11 +2,16 @@
 const { sortearDezenas, sortearJogoDoBicho } = require('./functions');
 
 // Declara e usa as ferramentas
-const { Client, MessageMedia, NoAuth, LocalAuth } = require('whatsapp-web.js');
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+
 // Cria o client local
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: false,
+        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+    }
 });
 
 // Gera o QRCode para login no whatsapp
@@ -23,22 +28,34 @@ client.on('ready', () => {
 client.on('message', async (message) => {
     console.log('Mensagem recebida: ', message.body);
 
+    // É possível citar a pessoa que enviou a mensagem
+    const chat = await message.getChat();
+    let user = await message.getContact();
+
+    if (message.body.toLowerCase() === '!oi') {
+        await chat.sendMessage(`Olá @${user.id.user}!`, { mentions: [user] });
+    }
+
+    if (message.body.toLowerCase() === '!lista3.0') {
+        await client.sendMessage(message.from, 'Segue a URL do vídeo solicitado: https://www.youtube.com/watch?v=Yl9SoE7N2yo');
+    }
+
+    if (message.body.toLowerCase() === '!lista2.0') {
+        await client.sendMessage(message.from, 'Segue a URL do vídeo solicitado: https://www.youtube.com/watch?v=JDO9xha81HQ');
+    }
+
     if (message.body.toLowerCase() === '!ping') {
         await client.sendMessage(message.from, 'pong');
     }
 
     if (message.body.toLowerCase() === '!megumin') {
-        const media = MessageMedia.fromFilePath('./images/megumin.jpeg')
-        const chat = await message.getChat()
-        await client.sendMessage(message.from, 'A mais gostosa, simpática e divertida feiticeira de Konosuba');
-        chat.sendMessage(media)
+        const media = MessageMedia.fromFilePath('./images/megumin.jpeg');
+        await chat.sendMessage(media, { caption: 'A mais gostosa, simpática e divertida feiticeira de Konosuba' });
     }
 
-    if (message.body.toLowerCase() === '!l'){
-        const media = MessageMedia.fromFilePath('./images/fazOL.jpg')
-        const chat = await message.getChat()
-        await client.sendMessage(message.from, 'Mantenha a calma guerreiro, lembre-se você tem culpa nisso, agora siga o conselho da imagem: ')
-        chat.sendMessage(media)
+    if (message.body.toLowerCase() === '!l') {
+        const media = MessageMedia.fromFilePath('./images/fazOL.jpg');
+        await chat.sendMessage(media, { caption: 'Mantenha a calma guerreiro, lembre-se você tem culpa nisso, agora siga o conselho da imagem' });
     }
 
     if (message.body.toLowerCase() === '!preco') {
@@ -51,17 +68,16 @@ client.on('message', async (message) => {
         await client.sendMessage(message.from, 'Algum dos nossos serviços se enquadra na sua demanda?');
     }
 
-     if (message.body.toLowerCase() === '!megasena'){
-         var sorteio = sortearDezenas()
-         await client.sendMessage(message.from, 'Seu palpite da Mega Sena é: ' + sorteio.join(', '));
-     }
+    if (message.body.toLowerCase() === '!megasena') {
+        var sorteio = sortearDezenas();
+        await client.sendMessage(message.from, 'Seu palpite da Mega Sena é: ' + sorteio.join(', '));
+    }
 
-     if (message.body.toLowerCase() === '!bicho'){
-        var resposta = await sortearJogoDoBicho()
+    if (message.body.toLowerCase() === '!bicho') {
+        var resposta = await sortearJogoDoBicho();
         await client.sendMessage(message.from, 'Seu palpite para o jogo do bicho é:\n ' + resposta);
-     }
+    }
 
 });
-
 
 client.initialize();
